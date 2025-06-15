bb001f823601df6dd7b078c6ac5cc773ef1917ae
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2025-06-15 14:24:14 +0900

    update

---
+++ sync-version.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+// package.jsonを読み込み
+const packageJson = require('../package.json');
+
+// tauri.conf.jsonを読み込み
+const tauriConfPath = path.join(__dirname, '../src-tauri/tauri.conf.json');
+const tauriConf = require(tauriConfPath);
+
+// バージョンを同期
+if (tauriConf.package) {
+  tauriConf.package.version = packageJson.version;
+} else {
+  tauriConf.version = packageJson.version;
+}
+
+// ファイルを書き込み
+fs.writeFileSync(tauriConfPath, JSON.stringify(tauriConf, null, 2) + '\n');
+
+console.log(`Version synced to ${packageJson.version}`);
